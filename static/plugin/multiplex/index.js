e7d0fad4e9d560032ae944fdf657237200bf9c97
Ross Kukulinski <ross@kukulinski.com>
2015-01-26 11:29:47 -0800

    Fix app server

--- static/plugin/multiplex/index.js
+++ static/plugin/multiplex/index.js
@@ -24,7 +24,7 @@ io.sockets.on('connection', function(socket) {
 });
 
 app.configure(function() {
-	[ 'css', 'js', 'plugin', 'lib' ].forEach(function(dir) {
+	[ 'static/css', 'static/js', 'static/plugin', 'static/lib', 'static/img' ].forEach(function(dir) {
 		app.use('/' + dir, staticDir(opts.baseDir + dir));
 	});
 });
